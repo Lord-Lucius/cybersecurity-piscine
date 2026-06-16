c5ea9587d5ef5058a95e5145700eed95fa451de8
luluzuri <luluzuri@student.42.fr>
2026-06-16 14:24:48 +0200

    [feature] finish get_page_links and get_page_images need save_image and scrape_page

--- Cybersecurity-arachnida-Web/ex00/spider.py
+++ Cybersecurity-arachnida-Web/ex00/spider.py
@@ -1,5 +1,12 @@
 import sys
 import re
+import requests
+from bs4 import BeautifulSoup
+
+class CustomsErrors:
+    class StatusCodeError(Exception):
+        def __init__(self, message):
+            super().__init__(message)
 
 def handle_options():
     recursive_flag = False
@@ -42,17 +49,48 @@ def handle_options():
     return recursive_flag, depth_len, save_path, url_to_scrap
 
 
-def scrape_page(url, save_path, recursive_flag, depth_len):
+def get_page_images(text):
+    soup = BeautifulSoup(text, "html.parser")
+    images_raw = soup.find_all('img')
+    img_set = set()
+    img_set.update(x.get('src') for x in images_raw if x.get('src'))
+    if img_set == {}:
+        return set()
+    return img_set
+
+def get_page_links(text):
+    soup = BeautifulSoup(text, "html.parser")
+    links = soup.find_all('a')
+    links_set = set()
+    links_set.update(x.get('href') for x in links if x.get('href'))
+    if links_set == {}:
+        return set()
+    return links_set
+
+def save_image():
     pass
 
+def scrape_page(url, save_path, current_depth, max_depth, visited):
+    r = requests.get(url, timeout=(2, 2))
+    if r.status_code != 200:
+        return
 
 def main():
     try:
         recursif_mod, depth_len, save_path, url_to_scrap = handle_options()
+        print("============================ HEADER =============================")
         print(f"{recursif_mod} :: {depth_len} :: {save_path} :: {url_to_scrap}")
-        scrape_page(url_to_scrap, save_path, recursif_mod, depth_len)
+        print("=================================================================\n\n")
+        if not recursif_mod:
+            scrape_page(url_to_scrap, save_path, 0, 0, set())
+        else:
+            scrape_page(url_to_scrap, save_path, 0, depth_len, set())
     except AssertionError as e:
         print(f"AssertionError: {e}")
+    except requests.exceptions.ConnectionError as e:
+        print(f"requests error ({e})")
+    except CustomsErrors.StatusCodeError as e:
+        print(f"status code error ({e})")
 
 if __name__ == "__main__":
     main()

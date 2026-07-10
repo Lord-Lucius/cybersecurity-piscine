19b9299ed1ed9fde3c60bbcb9625a2ecc9d6fb11
luluzuri <luluzuri@student.42.fr>
2026-07-10 13:41:47 +0200

    [feature] added local compilation to test the program

---
+++ inquisitor/include/utils.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/10 13:03:02 by luluzuri          #+#    #+#             */
+/*   Updated: 2026/07/10 13:08:30 by luluzuri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void usage(char *msg);
+void error(char *msg, int error_code);
+
+#endif

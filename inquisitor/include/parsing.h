9b7d30d2efaafe1119dd0125accbc5f7a37961bc
luluzuri <luluzuri@student.42.fr>
2026-07-12 13:18:33 +0200

    [feature] starting parsing

--- inquisitor/include/parsing.h
+++ inquisitor/include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 13:52:59 by luluzuri          #+#    #+#             */
-/*   Updated: 2026/07/10 14:01:59 by luluzuri         ###   ########.fr       */
+/*   Updated: 2026/07/11 08:53:00 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #define PARSING_H
 
 #include "inquisitor.h"
+
+int parse_arguments(int ac, char **av, t_config *config);
 
 int is_ipv4(char *src);
 int is_mac_addr(char *src);
--- inquisitor/src/main.c
+++ inquisitor/src/main.c
@@ -6,14 +6,18 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 11:02:46 by luluzuri          #+#    #+#             */
-/*   Updated: 2026/07/10 14:05:33 by luluzuri         ###   ########.fr       */
+/*   Updated: 2026/07/11 08:55:36 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+#include "parsing.h"
 #include "inquisitor.h"
 
-int main(void) {
+int main(int ac, char **av) {
+	t_config config;
+	parse_arguments(ac, av, &config);
+
 	return 0;
 }

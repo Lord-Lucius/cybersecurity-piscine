2695d7ef53cdf39957362dfe44a0491865ebad56
luluzuri <luluzuri@student.42.fr>
2026-07-19 10:00:45 +0200

    [feature] structures

---
+++ inquisitor/include/poisoning.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   poisoning.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/16 17:52:07 by luluzuri          #+#    #+#             */
+/*   Updated: 2026/07/16 17:54:15 by luluzuri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+typedef struct s_eth_header {
+
+} s_eth_header;
+
+typedef struct s_arp_msg {
+
+} t_arp_msg;
+
+typedef struct s_arp_frame {
+
+} __attribute__((__packed__)) t_arp_frame;

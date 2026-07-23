4d0df547a4f5b7681dfc03c74375584047addf38
luluzuri <luluzuri@student.42.fr>
2026-07-23 19:44:17 +0200

    [feature] poisoning

--- inquisitor/include/poisoning.h
+++ inquisitor/include/poisoning.h
@@ -6,7 +6,7 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 17:52:07 by luluzuri          #+#    #+#             */
-/*   Updated: 2026/07/20 15:18:05 by luluzuri         ###   ########.fr       */
+/*   Updated: 2026/07/23 19:43:48 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 #include <stdio.h>
 
 typedef struct s_eth_header {
-	unsigned char dst[6];
-	unsigned char src[6];
-	unsigned short type;
+	unsigned char mac_dst[6];
+	unsigned char mac_src[6];
+	unsigned short ether_type;
 } __attribute__((__packed__)) s_eth_header;
 
 typedef struct s_arp_msg {
-
+	unsigned short hardware_type;
+	unsigned short protocol_type;
+	unsigned char hardware_addr_len;
+	unsigned char protocol_addr_len;
 } t_arp_msg;
 
 typedef struct s_arp_frame {

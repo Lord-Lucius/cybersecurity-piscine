c73e134cc2e4db441692f929c5bbb899548e839c
luluzuri <luluzuri@student.42.fr>
2026-08-12 16:20:33 +0200

    [corr] put back the basic main file i had

--- vaccine/src/main.rs
+++ vaccine/src/main.rs
@@ -6,7 +6,7 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/12 12:20:12 by luluzuri          #+#    #+#             */
-/*   Updated: 2026/08/12 16:09:10 by luluzuri         ###   ########.fr       */
+/*   Updated: 2026/08/12 16:18:28 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ fn error(msg: String, code: i32) {
     process::exit(code);
 }
 
+/// Arg count should be between 2 and 6 because can go like:
+/// ["./vaccine", "-X", "POST", "-o", "res.txt", "http://h/?id=1"],
+/// and the program name is always here
 fn check_args() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        error("Not enought arguments".to_owned(), 1);
+    if args.len() < 2 && args.len() > 6 {
+        error("Bad arguments given".to_owned(), 1);
     }
     args
 }
